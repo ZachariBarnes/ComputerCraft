0bad72cd62e9c445e825f3eb81e28f39b5aefb88
Zachari Barnes <ZachariBarnes@yahoo.com>
2024-04-24 09:52:10 -0500

    Adding Locomotion library

---
+++ lib/locomotion.lua
@@ -0,0 +1,46 @@
+local module = {}
+DIRS = table.pack('N', 'E', 'S', 'W')
+
+function module.findIndex( table, value )
+    for i = 1, #table do
+        if table[i] == value then 
+            return i 
+        end
+    end
+    print( value ..' not found' ) 
+    if index == -1 then
+    print('ERROR. Invalid facing direction:', face)
+    print('Unable to proceed! Throwing Exception!')
+    error('ERROR. Invalid facing direction:', face)
+end
+
+function module.findFacingIndex( table, value )
+    index = module.findIndex(table, value)
+    if index == -1 then
+        print('ERROR. Invalid facing direction:', face)
+        print('Unable to proceed! Throwing Exception!')
+        error('ERROR. Invalid facing direction:', face)
+    end
+    return index
+end
+
+function module.turnL(face)
+    index = module.findFacingIndex(DIRS, face)
+    turtle.turnLeft()
+    index = index -1
+    if index < 1 then
+        index = 4
+    end
+end
+   
+function module.turnR(face)
+    index = module.findFacingIndex(DIRS, face)
+    turtle.turnLeft()
+    index = index + 1
+    if index > 4 then
+        index = 1
+    end
+end
+
+
+return module
